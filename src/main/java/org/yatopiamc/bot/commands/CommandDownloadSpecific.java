4b285af9347c674ca0e258aee610fa92fb25e834
Simon Gardling <titaniumtown@gmail.com>
2021-04-20 13:57:50 -0400

    cleanup

--- src/main/java/org/yatopiamc/bot/commands/CommandDownloadSpecific.java
+++ src/main/java/org/yatopiamc/bot/commands/CommandDownloadSpecific.java
@@ -10,9 +10,9 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import org.jetbrains.annotations.NotNull;
 import org.yatopiamc.bot.EmbedUtil;
 
-@CommandAliases("download|1.15.2|1.16.1|1.16.2|1.16.3|1.16.4|1.16.5")
+@CommandAliases("?download|?1.16.5")
 @CommandDescription("Download specific commands.")
-@CommandUsage("download|?1.15.2|?1.16.1|?1.16.2|?1.16.3|?1.16.4|?1.16.5")
+@CommandUsage("?download|?1.16.5")
 public class CommandDownloadSpecific extends Command {
 
   @Override
@@ -22,39 +22,8 @@ public class CommandDownloadSpecific extends Command {
     switch (alias) {
       case "download":
         channel
-            .sendMessage(
-                EmbedUtil.withAuthor(context.getAuthor())
-                    .setDescription(
-                        "Run `?1.15.2` or `?1.16.1` or `?1.16.2` or `?1.16.3` or `?1.16.4` or `?1.16.5`")
-                    .build())
-            .queue();
-        break;
-      case "1.15.2":
-        channel
-            .sendMessage(
-                "<https://github.com/YatopiaMC/Yatopia/actions?query=branch%3Aver%2F1.15.2>")
-            .queue();
-        break;
-      case "1.16.1":
-        channel
-            .sendMessage(
-                "<https://github.com/YatopiaMC/Yatopia/actions?query=branch%3Aver%2F1.16.1>")
-            .queue();
-        break;
-      case "1.16.2":
-        channel
-            .sendMessage("<https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.2/>")
-            .queue();
-        break;
-      case "1.16.3":
-        channel
-            .sendMessage("<https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.3/>")
-            .queue();
-        break;
-      case "1.16.4":
-        channel
-            .sendMessage("<https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.4/>")
-            .queue();
+          .sendMessage("<https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.5/>")
+          .queue();
         break;
       case "1.16.5":
         channel
