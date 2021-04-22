0c61c3538bc0c1d3a20983078a748bc2d691f0fa
Simon Gardling <titaniumtown@gmail.com>
2021-04-22 14:06:34 -0400

    cleanup

--- src/main/java/org/yatopiamc/bot/commands/CommandCaptchaTest.java
+++ src/main/java/org/yatopiamc/bot/commands/CommandCaptchaTest.java
@@ -8,27 +8,6 @@ import com.mrivanplays.jdcf.data.CommandDescription;
 import com.mrivanplays.jdcf.data.CommandUsage;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import org.jetbrains.annotations.NotNull;
-
-
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.yatopiamc.bot.timings.TimingsMessageListener;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 import org.yatopiamc.bot.captcha.YatoCaptchaTest;
 
 @CommandAliases("captchatest")
